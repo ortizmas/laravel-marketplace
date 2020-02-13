6eca33616a644cea6a7e19d46d1a300dd8d49729
Eber Ortiz <ortizmas14@gmail.com>
2020-02-13 10:52:03 -0300

    Criando novo branch

---
+++ resources/views/git-branch-blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.front')
+
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <h2>Git criando novo branch</h2>
+        <hr>
+    </div>
+
+    <div class="col-12">
+        <code>git checkout -b soportewebsites</code>
+    </div>
+</div>
+
+@endsection
